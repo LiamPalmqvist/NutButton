e52b09422f711c197b643e87a8ee6172cab0eeea
Liam Palmqvist <Liam.palmqvist@icloud.com>
2024-07-31 15:40:16 +0100

    Initial Commit

---
+++ BetterNutButton/BetterNutButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterNutButtonApp.swift
+//  BetterNutButton
+//
+//  Created by Liam Palmqvist on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterNutButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
